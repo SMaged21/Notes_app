dd087aeb4211745a52c0db3baadc75a6ac55e133
SMaged21 <zweensam@gmail.com>
2025-01-04 18:34:37 +0200

    Add addNote method to NoteCubit for saving notes in Hive

--- lib/cubit/addNote/add_note_cubit.dart
+++ lib/cubit/addNote/add_note_cubit.dart
@@ -1,6 +1,22 @@
+import 'dart:math';
+
+import 'package:hive/hive.dart';
+import 'package:notes_app/constants.dart';
 import 'package:notes_app/cubit/addNote/add_note_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/models/note_model.dart';
 
 class NoteCubit extends Cubit<NoteState> {
   NoteCubit() : super(NoteInitial());
+
+  void addNote(NoteModel note) async {
+    emit(NoteLoading());
+    try {
+      var notesBox = Hive.box<NoteModel>(kNotesBox);
+      emit(NoteSuccess());
+      await notesBox.add(note);
+    } catch (e) {
+      emit(NoteError(e.toString()));
+    }
+  }
 }
